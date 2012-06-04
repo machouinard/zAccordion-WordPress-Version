80f004db0fcdd946b4d79457950dc53bf4e7697f
Brad Parbs <bradparbs@Architect.local>
2012-06-04 13:17:56 -0500

    zAccordion WordPress-ed

---
+++ init.php
@@ -0,0 +1,37 @@
+<?
+/*
+Plugin Name: zAccordion WordPress-ed
+Description: Implementation of zAccordion
+Version: 1.0
+Author: Brad Parbs; OnYourMark, LLC
+Author URI: http://onyourmark.com
+License: GPL2
+*/
+
+function zaccordionSetup(){
+
+wp_enqueue_script("jquery");
+
+wp_enqueue_script(
+  "jquery.zaccordion", WP_PLUGIN_URL."/zAccordion/js/jquery.zaccordion.min.js", 
+  array("jquery"), "",1);	
+
+wp_enqueue_script(
+  "jquery.easing", WP_PLUGIN_URL."/zAccordion/js/jquery.easing.1.3.js", 
+  array("jquery","jquery.zaccordion"), "",1);
+}
+
+add_action('wp_footer', 'zaccordionSetup',5); 
+
+function jQueryFun(){
+
+wp_enqueue_script(
+  "slider.js", WP_PLUGIN_URL."/zAccordion/js/slider.js", 
+  array("jquery","jquery.zaccordion"), "",1);
+
+}
+
+add_action('wp_head','jQueryFun',6);
+
+
+?>
---
+++ readme.md
@@ -0,0 +1,17 @@
+zAccordion was originally made by Nate Armagost, and can be found at : http://www.armagost.com/zaccordion or https://github.com/natearmagost/zAccordion
+
+
+<ul id="featured">
+	<li>
+		<image src="/images/1.jpg" width="600" height="310" alt="Old School Diner" />
+	</li>
+	<li>
+		<image src="/images/2.jpg" width="600" height="310" alt="Vintage Pool" />
+	</li>
+	<li>
+		<image src="/images/3.jpg" width="600" height="310" alt="Retro Gas Pump" />
+	</li>
+	<li>
+		<image src="/images/4.jpg" width="600" height="310" alt="Old Car Steering Wheel" />
+	</li>
+</ul>
